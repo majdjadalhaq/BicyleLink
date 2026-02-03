6f03704c2672f74b10f5a2c8992c068a98966262
stasel <2033301+stasel@users.noreply.github.com>
2026-02-03 14:02:54 +0100

    Changed the background color on the home page header

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,9 +2,13 @@ import TEST_ID from "./Home.testid";
 import hyfLogo from "../../assets/hyf-logo.png";
 
 const Home = () => {
+  const headerStyle = {
+    background: "skyblue",
+    padding: "10px",
+  };
   return (
     <div data-testid={TEST_ID.container}>
-      <h1>This is the homepage</h1>
+      <h1 style={headerStyle}>This is the homepage</h1>
       <p>Good luck with the project!</p>
       <img src={hyfLogo} alt="HackYourFuture Logo" style={{ width: "200px" }} />
     </div>
